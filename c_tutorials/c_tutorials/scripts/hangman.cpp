6a04a5252bdb150c5c13676e2476c905a823ca82
SpacePanda-42 <77905853+SpacePanda-42@users.noreply.github.com>
2022-12-30 00:23:15 -0500

    guess letter and other changes

--- c_tutorials/c_tutorials/scripts/hangman.cpp
+++ c_tutorials/c_tutorials/scripts/hangman.cpp
@@ -11,23 +11,69 @@ class Game {
 private:
     bool win = false;
     bool correctGuess = false;
-    int numLives = 10;
 
 public:
-    void startGame(vector<string> dictionary);
+    string startGame(vector<string> dictionary);
     void endGame();
-    void guessLetter();
+    void guessLetter(vector<string> remaining_letters, vector<string> prevous_guesses);
     void writeDictionary(); // Dummy function that was used as a test for writing to a file
+    void loseGame();
+    void runGame();
     vector<string> loadDictionary();
+    int nLives = 5;
+    string remainingLetters;
+    string current_progress;
 };
 
-void Game::startGame(vector<string> dictionary){
-    cout << "Guess a letter" << endl;
+string Game::startGame(vector<string> dictionary){
+    // Pick a word at random from the dictionary
+    string word;
     srand(time(0));
     int random_pos = rand() % dictionary.size();
-    cout << random_pos << endl;
-    cout << dictionary[random_pos] << endl;
+    // cout << random_pos << endl;
+    // cout << dictionary[random_pos] << endl;
+    word = dictionary[random_pos];
+    return(word);
 };
+
+void Game::guessLetter(vector<string> remaining_letters, vector<string> previous_guesses){
+    string letter_guess;
+    bool unique_guess = false;
+    cout << "Guess a letter" << endl;
+    cin >> letter_guess;
+
+    // Check if we've already guessed the letter
+    while(!unique_guess){
+        if(*find(previous_guesses.begin(), previous_guesses.end(), letter_guess) == letter_guess){
+            unique_guess = false;
+            cout << "You already guessed that letter." << endl;
+            cout << "Guess another letter" << endl;
+            cin >> letter_guess;
+        }else{
+            unique_guess = true;
+        }
+    }
+    
+    // Check if the guessed letter is one of the remaining letters of the word
+    if(*find(remaining_letters.begin(), remaining_letters.end(), letter_guess) == letter_guess){
+        cout << "Letter was in the word" << endl;
+    }else{
+        cout << "Letter wasn't in the word" << endl;
+        nLives -= 1;
+    }
+
+};
+
+void Game::runGame(){
+    while(nLives > 0){
+        guessLetter()
+    }
+
+}
+
+void Game::loseGame(){
+    cout << "You lose" << endl;
+}
 
 void Game::writeDictionary(){
     fstream newfile;
@@ -70,8 +116,11 @@ vector<string> Game::loadDictionary(){
 
 int main(){
     Game game;
+    string word;
     // game.startGame();
     vector<string> dictionary;
     dictionary = game.loadDictionary();
-    game.startGame(dictionary);
+    word = game.startGame(dictionary);
+    game.runGame();
+
 }
